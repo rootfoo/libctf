d7b6c0dabab7fb8bce5793f0d2c3adf200f92c68
meta <Marcus Hodges>
2018-05-15 15:55:30 -0700

    added Sock.readuntil() and ANSI colored console output

--- sock.py
+++ sock.py
@@ -2,145 +2,188 @@ import socket
 import select
 import sys
 from data import hexdump
-
+import ansi 
 
 class Sock(object):
 
-	"""
-	wrapper for a socket that's smart
-	functions for pretty printing the dialog
-	read_until_string
-	read_all (for slow connections)
-	"""
-	def __init__(self, host, port, family=socket.AF_INET, stype=socket.SOCK_STREAM, timeout=.2, verbose=False):
-		self.socket = socket.socket(family, stype)
-		self.socket.settimeout(timeout)
-		self.socket.connect((host,port))
-		self._family = family
-		self._stype = stype
-		self._host = host
-		self._port = port
-		self.history = []
-		self.verbose = verbose
+    """
+    wrapper for a socket that's smart
+    functions for pretty printing the dialog
+    read_until_string
+    read_all (for slow connections)
+    """
+    def __init__(self, host, port, family=socket.AF_INET, stype=socket.SOCK_STREAM, timeout=.1, verbose=True, newline='', color=True):
+        self.socket = socket.socket(family, stype)
+        self.socket.settimeout(timeout)
+        self.socket.connect((host,port))
+        self._timeout = timeout
+        self._family = family
+        self._stype = stype
+        self._host = host
+        self._port = port
+        self._tail = ""
+        self._recv_color = ansi.ANSI_F_CYAN if color else ''
+        self._send_color = ansi.ANSI_F_MAGENTA if color else ''
+        self.history = []
+        self.verbose = verbose
+        self.newline = newline
 
 
-	def read(self, count=None):
-		data = ''
-		if count:
-			data = self._read(count)
-		else:
-			data = self._readall()
-		if self.verbose:
-			print hexdump(data)
-		return data
+    def recv(self, count=None, timeout=None):
+        """Alias for read()"""
+        return self.read(count)
 
 
-	def recv(self, count=None):
-		return self.read(count)
+    def read(self, count=None, timeout=None):
+        """read up to count bytes, if specified, otherwise read all data available"""
+        if timeout:
+            # set timeout for this request
+            self.socket.settimeout(timeout)
+        data = self._tail
+        if count:
+            data = self._read(count)
+        else:
+            data = self._readall()
+        if self.verbose:
+            sys.stdout.write(self._recv_color + data + ansi.ANSI_ENDC)
+            #print hexdump(data)
+
+        # reset timeout, append to history, truncate tail
+        self.socket.settimeout(self._timeout)
+        self.history.append(('>',data))
+        self._tail = ''
+        
+        return data
 
 
-	def _read(self, count=4096):
-		data = ''
-		try:
-			data = self.socket.recv(count)
-			self.history.append(('>',data))
+    def _read(self, count=4096):
+        data = ''
+        try:
+            data = self.socket.recv(count)
 
-		except socket.timeout:
-			# timeout doesn't necesarily mean the connection is closed
-			pass
+        except socket.timeout:
+            # timeout doesn't necesarily mean the connection is closed
+            pass
 
-		return data
+        return data
 
-	def _readall(self):
-		# read all data from sock
-		data = ""
-		chunk = ""
-		flag = True
-		while flag:
-			try:
-				chunk = self.socket.recv(1024)
-			except socket.timeout:
-				chunk = ""
-			finally:
-				if len(chunk) > 0:
-					data += chunk
-					chunk = ""
-				else:
-					flag = False
+    def _readall(self):
+        # read all data from sock
+        data = ""
+        chunk = ""
+        flag = True
+        while flag:
+            try:
+                chunk = self.socket.recv(1024)
+            except socket.timeout:
+                chunk = ""
+            finally:
+                if len(chunk) > 0:
+                    data += chunk
+                    chunk = ""
+                else:
+                    flag = False
 
-		self.history.append(('>',data))
-		return data
+        return data
 
 
-	def send(self, data):
-		self.write(data)
+    def recvuntil(self, premark, postmark="\n"):
+        """
+        Read until premark in mesg. Return string between premark and postmark.
+        TODO: add support for regex
+        """
+        # self._tail caches data read but not yet returned. May be part of next read
+        while not premark in self._tail:
+            self._tail += self._readall()
 
-	def sr(self, data=''):
-		"""send(data) && return recv()"""
-		if data:
-			self.write(data)
-		return self.read()
+        start = self._tail.find(premark) + len(premark)
+        end = self._tail.find(postmark, start)
+        sub = self._tail[start:end]
+        self._tail = self._tail[end:]
 
-
-	def write(self, data):
-		try:
-			self.socket.send(data)
-			self.history.append(('<',data))
-			
-			if self.verbose:
-				print hexdump(data)
-			return data
-
-		except Exception as e:
-			self.socket.close()
-			self._closed = True
+        return sub
 
 
-	def close(self):
-		self.socket.close()
-
-	def reconnect(self):
-		self.socket.close()
-		self.socket = socket.socket(self._family, self._stype)
-		self.socket.settimeout(2)
-		self.socket.connect((self._host,self._port))
+    def send(self, data):
+        """alias for write()"""
+        self.write(data)
 
 
-	def interact(self, newline="\n"):
+    def sendline(self, data):
+        """write(data + "\n")"""
+        self.write(data, newline="\n")
 
-		while True:
-			try:
-				# It would be nice to have a prompt, but it's tricky to add
-				#sys.stdout.write('$> ')
-				#sys.stdout.flush()
 
-				# Wait for input from stdin & socket
-				input_ready,output_ready,except_ready = select.select([sys.stdin, self.socket], [],[])
+    def sr(self, data=''):
+        """send(data) && return recv()"""
+        if data:
+            self.write(data)
+        return self.read()
 
-				# loop over the file handles with input
-				for i in input_ready:
-					if i == sys.stdin:
-						data = sys.stdin.readline()
-						# patch up newlines
-						if not data.endswith(newline):
-							data = data.strip('\n') + newline
 
-						if data:
-							self.history.append(('<', data))
-							self.socket.send(data)
-					elif i == self.socket:
-						data = self.socket.recv(1024)
-						if data:
-							self.history.append(('>', data))
-							sys.stdout.write(data)
-							sys.stdout.flush()
+    def write(self, data, newline=''):
+        newline = newline or self.newline
+        try:
+            data += newline
+            self.socket.send(data)
+            self.history.append(('<',data))
 
-			except KeyboardInterrupt:
-				break
+            if self.verbose:
+                sys.stdout.write(self._send_color + data + ansi.ANSI_ENDC)
+                #print hexdump(data)
 
-			except socket.error as msg:
-				print msg
-				break
+            return data
+
+        except Exception as e:
+            self.socket.close()
+            self._closed = True
+
+
+    def close(self):
+        self.socket.close()
+
+    def reconnect(self):
+        self.socket.close()
+        self.socket = socket.socket(self._family, self._stype)
+        self.socket.settimeout(2)
+        self.socket.connect((self._host,self._port))
+
+
+    def interact(self, newline="\n"):
+
+        while True:
+            try:
+                # It would be nice to have a prompt, but it's tricky to add
+                #sys.stdout.write('$> ')
+                #sys.stdout.flush()
+
+                # Wait for input from stdin & socket
+                input_ready,output_ready,except_ready = select.select([sys.stdin, self.socket], [],[])
+
+                # loop over the file handles with input
+                for i in input_ready:
+                    if i == sys.stdin:
+                        data = sys.stdin.readline()
+                        # patch up newlines
+                        if not data.endswith(newline):
+                            data = data.strip('\n') + newline
+
+                        if data:
+                            self.history.append(('<', data))
+                            self.socket.send(data)
+                    elif i == self.socket:
+                        data = self.socket.recv(1024)
+                        if data:
+                            self.history.append(('>', data))
+                            sys.stdout.write(data)
+                            sys.stdout.flush()
+
+            except KeyboardInterrupt:
+                break
+
+            except socket.error as msg:
+                print msg
+                break
 
 
 
